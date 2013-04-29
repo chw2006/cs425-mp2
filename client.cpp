a427881425670d2913a86bec3256d01a9a0bd3ba
David Huang <huang157@gelib-057-14.ews.illinois.edu>
2013-04-29 14:21:27 -0500

    Add manual testing for client

--- client.cpp
+++ client.cpp
@@ -2,6 +2,9 @@
 #include "replicas.h"
 
 #include <iostream>
+#include <sstream>
+#include <vector>
+#include <map>
 
 using namespace mp2;
 using namespace std;
@@ -27,6 +30,71 @@ int main(int argc, char **argv) {
 	} catch (ReplicaError e) {
 			cerr << e.message << endl;
 			return 1;
+	}
+
+	// get user input
+	string input;
+	map<string, shared_ptr<StateMachine> > machines;
+	while(1) {
+		try {
+			// receive input from terminal
+			getline(cin, input);
+
+			// parse inputs
+			vector<string> args;
+		    istringstream iss(input);
+		    do {
+		        string sub;
+		        iss >> sub;
+		        if(sub != "")
+		        	args.push_back(sub);
+		    } while (iss);
+
+		    // do operations and loop
+		    if(args.size() < 1)
+		    	continue;
+		    // create new machine: create [name] [initial_state]
+		    if(args[0] == "create") {
+		    	if(args.size() != 3) {
+		    		cout << "Expected 3 arguments" << endl;
+		    	}else{
+		    		cout << "Creating machine " << args[1] << endl;
+		    		shared_ptr<StateMachine> temp = frontEnd.create(args[1], args[2]);
+		    		machines.insert(pair<string, shared_ptr<StateMachine> >(args[1], temp));
+		    	}
+		    // get current state: get [name]
+		    }else if(args[0] == "get") {
+		    	if(args.size() != 2) {
+		    		cout << "Expected 2 arguments" << endl;
+		    	}else{
+		    		cout << "Getting state from machine " << args[1] << endl;
+		    		cout << machines[args[1]]->getState() << endl;
+		    	}
+		    // apply operation: apply [name] [operation]
+		    }else if(args[0] == "apply") {
+		    	if(args.size() != 3) {
+		    		cout << "Expected 3 arguments" << endl;
+		    	}else{
+		    		cout << "Applying operation " << args[2] << " to machine " << args[1] << endl;
+		    		cout << machines[args[1]]->apply(args[2]) << endl;
+		    	}
+		    // remove machine: remove [name]
+		    }else if(args[0] == "remove") {
+		    	if(args.size() != 2) {
+		    		cout << "Expected 2 arguments" << endl;
+		    		break;
+		    	}
+		    	cout << "Removing machine " << args[1] << endl;
+		    	frontEnd.remove(args[1]);
+		    // invalid inputs
+		    }else if(args[0] == "exit") {
+		    	return 0;
+		    }else{
+		    	cout << "Invalid arguments" << endl;
+		    }
+		} catch (ReplicaError e) {
+			cerr << e.message << endl;
+		}
 	}
 
 	return 0;
